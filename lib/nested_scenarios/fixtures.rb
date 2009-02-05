9b7e33a0106c58905dccc680fd096a2193ad8ac2
José Valim <jose.valim@gmail.com>
2009-02-05 15:50:35 +0100

    Nested Scenarios Rails 2.3 compatible.

--- lib/nested_scenarios/fixtures.rb
+++ lib/nested_scenarios/fixtures.rb
@@ -4,14 +4,40 @@ class Fixtures < (RUBY_VERSION < '1.9' ? YAML::Omap : Hash)
   end
 end
 
-module Test #:nodoc:
-  module Unit #:nodoc:
-    class TestCase #:nodoc:
-      superclass_delegating_accessor :scenario_path
-      superclass_delegating_accessor :load_root_fixtures
-      self.load_root_fixtures = false
+module ActiveRecord #:nodoc:
+  module TestFixtures #:nodoc:
+  
+    def self.included(base)
+      base.class_eval do
+        setup :setup_fixtures
+        teardown :teardown_fixtures
 
-      def self.scenario(scenario_name = nil, options = {})
+        superclass_delegating_accessor :fixture_path
+        superclass_delegating_accessor :fixture_table_names
+        superclass_delegating_accessor :fixture_class_names
+        superclass_delegating_accessor :use_transactional_fixtures
+        superclass_delegating_accessor :use_instantiated_fixtures   # true, false, or :no_instances
+        superclass_delegating_accessor :pre_loaded_fixtures
+
+        self.fixture_table_names = []
+        self.use_transactional_fixtures = false
+        self.use_instantiated_fixtures = true
+        self.pre_loaded_fixtures = false
+
+        self.fixture_class_names = {}
+
+        superclass_delegating_accessor :scenario_path
+        superclass_delegating_accessor :load_root_fixtures
+        superclass_delegating_accessor :root_table_names
+        superclass_delegating_accessor :scenario_table_names
+        self.load_root_fixtures = false
+      end
+
+      base.extend ClassMethods
+    end
+  
+    module ClassMethods
+      def scenario(scenario_name = nil, options = {})
         case scenario_name
           when Hash
             self.load_root_fixtures = scenario_name.delete(:root) unless scenario_name[:root].nil?
@@ -27,12 +53,12 @@ module Test #:nodoc:
         self.fixtures(:all)
       end
 
-      def self.fixtures(*table_names)
+      def fixtures(*table_names)
         if table_names.first == :all
-          @@root_table_names = self.load_root_fixtures ? load_table_names_in_path(self.fixture_path) : []
-          @@scenario_table_names = self.scenario_path ? load_table_names_in_path(self.scenario_path) : []
+          self.root_table_names = self.load_root_fixtures ? load_table_names_in_path(self.fixture_path) : []
+          self.scenario_table_names = self.scenario_path ? load_table_names_in_path(self.scenario_path) : []
 
-          table_names = @@root_table_names + @@scenario_table_names
+          table_names = self.root_table_names + self.scenario_table_names
           table_names.uniq!
         else
           table_names = table_names.flatten.map { |n| n.to_s }
@@ -45,51 +71,53 @@ module Test #:nodoc:
       end
 
       private
-        def self.load_table_names_in_path(path)
+        def load_table_names_in_path(path)
           table_names = Dir["#{path}/*.yml"] + Dir["#{path}/*.csv"]
           table_names.map! { |f| File.basename(f).split('.')[0..-2].join('.') }
           return table_names
         end
-
-        def load_fixtures
-          @loaded_fixtures = {}
-
-          if self.load_root_fixtures
-            root_fixtures = Fixtures.create_fixtures(self.fixture_path, @@root_table_names, fixture_class_names)
-          end
-
-          if self.scenario_path
-            scenario_fixtures = Fixtures.create_fixtures(self.scenario_path, @@scenario_table_names, fixture_class_names)
-          end
-
-          [root_fixtures, scenario_fixtures].each do |fixtures|
-            next if fixtures.nil?
-
-            if fixtures.instance_of?(Fixtures)
-              @loaded_fixtures[fixtures.table_name] = fixtures
-            else
-              fixtures.each { |f| @loaded_fixtures[f.table_name] = f }
-            end
-          end
-        end
-
-        def teardown_fixtures
-          return unless defined?(ActiveRecord) && !ActiveRecord::Base.configurations.blank?
-
-          Fixtures.destroy_fixtures(self.fixture_table_names)
-
-          unless use_transactional_fixtures?
-            Fixtures.reset_cache
-          end
-
-          # Rollback changes if a transaction is active.
-          if use_transactional_fixtures? && ActiveRecord::Base.connection.open_transactions != 0
-            ActiveRecord::Base.connection.rollback_db_transaction
-            ActiveRecord::Base.connection.decrement_open_transactions
-          end
-
-          ActiveRecord::Base.clear_active_connections!
-        end
     end
+
+    private
+      def load_fixtures
+        @loaded_fixtures = {}
+
+        if self.load_root_fixtures
+          root_fixtures = Fixtures.create_fixtures(self.fixture_path, self.root_table_names, fixture_class_names)
+        end
+
+        if self.scenario_path
+          scenario_fixtures = Fixtures.create_fixtures(self.scenario_path, self.scenario_table_names, fixture_class_names)
+        end
+
+        [root_fixtures, scenario_fixtures].each do |fixtures|
+          next if fixtures.nil?
+
+          if fixtures.instance_of?(Fixtures)
+            @loaded_fixtures[fixtures.table_name] = fixtures
+          else
+            fixtures.each { |f| @loaded_fixtures[f.table_name] = f }
+          end
+        end
+      end
+
+      def teardown_fixtures
+        return unless defined?(ActiveRecord) && !ActiveRecord::Base.configurations.blank?
+
+        Fixtures.destroy_fixtures(self.fixture_table_names)
+
+        unless use_transactional_fixtures?
+          Fixtures.reset_cache
+        end
+
+        # Rollback changes if a transaction is active.
+        if use_transactional_fixtures? && ActiveRecord::Base.connection.open_transactions != 0
+          ActiveRecord::Base.connection.rollback_db_transaction
+          ActiveRecord::Base.connection.decrement_open_transactions
+        end
+
+        ActiveRecord::Base.clear_active_connections!
+      end
+
   end
 end
--- nested_scenarios.gemspec
+++ nested_scenarios.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name     = "nested_scenarios"
-  s.version  = "0.1.1"
+  s.version  = "0.2"
   s.date     = "2008-11-29"
   s.summary  = "FixtureScenarios, FixtureScenariosBuilder, Yaml and Ruby in one big mix for Rails"
   s.email    = "jose.valim@gmail.com"
